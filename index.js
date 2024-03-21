9563069f5ccef1e87366c670ba0430f3a8a7e04d
manvisingh3097 <singh.thakurmanvi@gmail.com>
2024-03-21 23:04:34 +0530

    Added swagger and eslint

--- index.js
+++ index.js
@@ -1,49 +1,44 @@
-var express = require('express');
-const colors = require('colors');
-const cors = express('cors');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
+var express = require("express");
+const colors = require("colors");
+const cors = express("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
 
-const { Org,Item,Pricing } = require('./models/models');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
+const { Org, Item, Pricing } = require("./models/models");
 
 //dot env  config
-dotenv.config()
+dotenv.config();
 
 //rest object
 var app = express();
 
 //middlewares
-app.use(cors)
+app.use(cors);
 app.use(express.json());
-app.use(morgan('dev'));
-
+app.use(morgan("dev"));
 
 //route
 
-app.use('/api/v1/test', require('./routes/testRoutes'))
+app.use("/api/v1/test", require("./routes/testRoutes"));
 
-app.get('/',(req, res) => {
-    return res
-    .status(200)
-    .send('<h1>Welcome to food delivery app</h1>');
+app.get("/", (req, res) => {
+  return res.status(200).send("<h1>Welcome to food delivery app</h1>");
 });
 
-app.post("/put_org", async (req,res) => {
-  const  newOrg = await Org.create({"name":"asd"});
-  return res
-  .status(200)
-  .send(newOrg)
+app.post("/put_org", async (req, res) => {
+  const newOrg = await Org.create({ name: "asd" });
+  return res.status(200).send(newOrg);
 });
 
-app.get("/get_all_orgs",async (req, res) => {
+app.get("/get_all_orgs", async (req, res) => {
   const results = await Org.findAll();
-  return res
-  .status(200)
-  .send(results)
+  return res.status(200).send(results);
 });
 
-app.post("/put_price", async (req,res) => {
+app.post("/put_price", async (req, res) => {
   const org_id = req.body.org_id;
   const item_id = req.body.item_id;
   const zone = req.body.zone;
@@ -51,20 +46,17 @@ app.post("/put_price", async (req,res) => {
   const km_price = req.body.km_price;
   const fix_price = req.body.fix_price;
   const newPricing = await Pricing.create({
-    "org_id":org_id,
-    "item_id":item_id,
-    "zone":zone,
-    "base_dis_in_km":base_dis_in_km,
-    "km_price":km_price,
-    "fix_price":fix_price
+    org_id: org_id,
+    item_id: item_id,
+    zone: zone,
+    base_dis_in_km: base_dis_in_km,
+    km_price: km_price,
+    fix_price: fix_price,
   });
-  return res
-  .status(200)
-  .send(newPricing)
+  return res.status(200).send(newPricing);
 });
 
 app.get("/get_price", async (req, res) => {
-  
   const zone = req.query.zone;
   const org_id = req.query.organisation_id;
   const total_dis = req.query.total_distance;
@@ -73,29 +65,26 @@ app.get("/get_price", async (req, res) => {
   let total_price;
 
   let org_details = await Pricing.findOne({
-    where:{
+    where: {
       org_id: parseInt(org_id),
       zone: zone,
-    }
+    },
   });
   const fix_price = org_details.fix_price;
   const km_price = org_details.km_price;
   const base_dis = org_details.base_dis_in_km;
 
   total_price = fix_price;
-  if (total_dis-base_dis >=0){
-    total_price = total_price + km_price*(total_dis-base_dis);
+  if (total_dis - base_dis >= 0) {
+    total_price = total_price + km_price * (total_dis - base_dis);
   }
-  return res
-  .status(200)
-  .send({"total_price":total_price})
+  return res.status(200).send({ total_price: total_price });
 });
-
 
 //port
 const PORT = process.env.PORT;
 
-
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument));
 //listen
 app.listen(PORT, function () {
   console.log(`Node server running on ${PORT}`.white.bgMagenta);
--- models/models.js
+++ models/models.js
@@ -1,79 +1,76 @@
-const { Sequelize,DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require("sequelize");
 // const sequelize = require('../database');
-require('dotenv').config();
+require("dotenv").config();
 
 const CONNECTION_STRING = process.env.CONNECTION_STRING;
 
+const sequelize = new Sequelize(CONNECTION_STRING, {
+  dialect: "postgres",
+  dialectOptions: { ssl: true },
+}); // Example for postgres
 
-const sequelize = new Sequelize(CONNECTION_STRING,
-{dialect:'postgres',dialectOptions:{'ssl':true} }   ) // Example for postgres
-
-const Org = sequelize.define('org', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
+const Org = sequelize.define("org", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 });
 
-const Item = sequelize.define('item', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    type: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    description: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
+const Item = sequelize.define("item", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  type: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 });
 
-const Pricing = sequelize.define('pricing', {
-    org_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-
-    },
-    item_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    zone: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    base_dis_in_km: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-    },
-    km_price: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-    },
-    fix_price: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-    },
+const Pricing = sequelize.define("pricing", {
+  org_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  item_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  zone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  base_dis_in_km: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  km_price: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  fix_price: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
 });
-
 
 async function init() {
-    await Item.sync();
-    await Org.sync();
-    
-    await Pricing.sync(); // force to create table if not exists
+  await Item.sync();
+  await Org.sync();
+
+  await Pricing.sync(); // force to create table if not exists
 }
 init();
-
-
 
 module.exports.Pricing = Pricing;
 module.exports.Item = Item;
